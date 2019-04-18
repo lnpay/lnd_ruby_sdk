976a8c145d136472a5074c0bc79b3e973a5dade1
Rasmus <rk@youngmedia.se>
2019-04-18 02:50:37 +0200

    Refactor to modules and instance methods instead of classes and objects

---
+++ lib/lightning/invoices.rb
@@ -0,0 +1,57 @@
+module Lightning
+  # Attempts to add a new invoice to the invoice
+  # database and returns a payment request.
+  #
+  # @option args [Integer] :value (0)
+  #   Amount in satoshis
+  #
+  # @option args [Integer] :expiry (3600)
+  #   Payment request expiry time in seconds.
+  #
+  # @option args [String] :memo ('')
+  #   An optional memo to attach along with the invoice.
+  #   Used for record keeping purposes for the invoices creator,
+  #   and will also be set in the description field of the encoded
+  #   payment request if the description_hash field is not being used.
+  #
+  # @example Create a new invoice
+  #   Lightning.add_invoice(value: 500, memo: '1x Cappuccino')
+  #
+  # @return [Lnrpc::AddInvoiceResponse] response
+  def self.add_invoice(**args)
+    value = args[:value]
+    expiry = args[:expiry]
+    memo = args[:memo]
+
+    stub.add_invoice(
+      Lnrpc::Invoice.new(value: value, expiry: expiry, memo: memo)
+    )
+  end
+
+  # Returns a list of all the invoices currently stored within
+  # the database. Any active debug invoices are ignored. It has full support
+  # for paginated responses, allowing users to query for specific invoices
+  # through their add_index. This can be done by using either the
+  # first_index_offset or last_index_offset fields included in the response
+  # as the index_offset of the next request. By default, the first 100
+  # invoices created will be returned. Backwards pagination is also
+  # supported through the Reversed flag.
+  #
+  # @example List all invoices
+  #   Lightning.list_invoices
+  #
+  # @return [Lnrpc::ListInvoiceResponse] response
+  def self.list_invoices(**args)
+    num_max_invoices = args[:num_max_invoices]
+    index_offset = args[:index_offset]
+    pending_only = args[:pending_only]
+    reversed = args[:reversed]
+
+    stub.list_invoices(
+      Lnrpc::ListInvoiceRequest.new(
+        num_max_invoices: num_max_invoices, index_offset: index_offset,
+        pending_only: pending_only, reversed: reversed
+      )
+    )
+  end
+end
--- lib/lightning/node.rb
+++ lib/lightning/node.rb
@@ -1,18 +1,13 @@
 module Lightning
-  class Node
-    class << self
-      include Lightning::Stub
-      # Returns general information concerning the lightning node
-      # including it's identity pubkey, alias, the chains it is connected
-      # to, and information concerning the number of open+pending channels.
-      #
-      # @example Receive node info
-      #   Lightning::Node.info #=> {:identity_pubkey=>... }
-      #
-      # @return [Lnrpc::GetInfoResponse] the response
-      def info
-        return_res stub.get_info(Lnrpc::GetInfoRequest.new)
-      end
-    end
+  # Returns general information concerning the lightning node
+  # including it's identity pubkey, alias, the chains it is connected
+  # to, and information concerning the number of open+pending channels.
+  #
+  # @example Receive node info
+  #   Lightning.get_info
+  #
+  # @return [Lnrpc::GetInfoResponse] the response
+  def self.get_node_info
+    stub.get_info(Lnrpc::GetInfoRequest.new)
   end
 end
